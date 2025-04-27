eecc095ae3be72cec902bab51a286f3c434c210e
picunf66666 <kopibarchik@gmail.com>
2025-04-27 19:32:30 +0300

    Add files via upload

--- vdkbskjbvs.py
+++ vdkbskjbvs.py
@@ -2,7 +2,7 @@ from pygame import *
 
 
 font.init()
-font1 = font.SysFont('Arial', 80)
+font1 = font.SysFont('Arial', 40)
 win_message = font1.render('PLAYER 1 WINS!', True, (255, 255, 255))
 lose_message = font1.render('PLAYER 2 WINS!', True, (180, 0, 0))
 
@@ -23,12 +23,14 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def update(self):
+    def update1(self):
         keys = key.get_pressed()
-        if keys[K_w] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_s] and self.rect.x < (win_width - 70):
-            self.rect.x += self.speed
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < (win_height - 70):
+            self.rect.y += self.speed
+    def update2(self):
+        keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
         if keys[K_DOWN] and self.rect.y < (win_height - 70):
@@ -39,16 +41,18 @@ win_width = 700
 win_height = 500
 window = display.set_mode((win_width, win_height))
 display.set_caption('Pingo Pongo')
-background = transform.scale(image.load('lanta-klong-nin.jpg'), (win_width, win_height)) 
+background = transform.scale(image.load('chto-takoe-nebo-1024x576.jpg'), (win_width, win_height)) 
 
-player1 = Player('Без названия.jpg', 50, 250, 5) 
-player2 = Player('Без названия.jpg', 650, 250, 5)  
+player1 = Player('Без названия.jpg', 50, 250, 6) 
+player2 = Player('Без названия.jpg', 600, 250, 6)  
 ball = GameSprite('m1000x1000.jpg', 350, 250, 5)
 
 clock = time.Clock()
 FPS = 60
 game = True
 finish = False
+speed_x = 1
+speed_y = 1
 
 while game:
     for e in event.get():
@@ -56,11 +60,27 @@ while game:
             game = False
 
     if not finish:
+        ball.rect.x +- speed_x
+        ball.rect.y +- speed_y
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
         window.blit(background, (0, 0))
-        player1.update()
+        player1.update1()
         player1.reset()
-        player2.update()
+        player2.update2()
         player2.reset()
+        ball.update()
+        ball.reset()
+    if  sprite.collide_rect(player1, ball) or sprite.collide_rect(player2, ball):
+        speed_x *= -1
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose_message, (200, 200))
+    if ball.rect.x > 700:
+        finish = True
+        window.blit(win_message, (200, 200))
+    if ball.rect.y > win_height-50 or ball.rect.y < 0:
+        speed_y *= -1
     
     display.update()
     clock.tick(FPS)
